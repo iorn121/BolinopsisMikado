6815131b165209b9442eec88d948c4ea767edae8
iorn121 <wmt.tkn.121@gmail.com>
2023-05-23 02:59:22 +0900

    update standard

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "gojelly/cmd"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func flagUsage() {
+usageText := `
+
+Usage:
+example command [arguments]
+The commands are:
+convhex    convert Number to Hex
+convbinary convert Number to binary
+Use "Example [command] --help" for more infomation about a command`
+
+fmt.Fprintf(os.Stderr, "%s\n\n", usageText)
+}
+
+var i int
+var s string
+var b bool
+
+func init() {
+	// Var() 関数の引数は以下の通り
+    // 第一引数は束縛する変数のポインタ
+    // 第二引数はフラグ名
+    // 第三引数はデフォルト値
+    // 第四引数はフラグの説明
+	flag.IntVar(&i, "i", 0, "数値" )
+	flag.StringVar(&s, "s", "default", "文字列")
+	flag.BoolVar(&b, "b", false, "真偽値")
+}
 
 func main() {
-	cmd.Execute()
+	flag.Parse()
+	fmt.Println(i, s, b)
 }
